91de7c978ee89a985db4f995ee31f3da782d159e
jor93 <robert.johner@gmail.com>
2016-10-18 12:47:49 +0200

    jor

---
+++ controllers/Class.elementsController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jor
+ * Date: 18.10.2016
+ * Time: 12:43
+ */
+
+
+class elementsController extends Controller {
+
+    public static function aboSelect(){
+        $answer = loginController::getAboByLanguage($_SESSION['lang']);
+        $length = count($answer);
+        for ($i = 0; $i < $length; ++$i) {
+            echo "<option value='" . $answer[$i] . "'" . ">" . $answer[$i][1] . "</option>";
+        }
+    }
+
+    public static function langSelect(){
+        // get language if possible
+        $language = loginController::getPreferredLanguage();
+        // variables for views
+        $array = array('en','de','fr');
+        $tr = array('English','Deutsch','Français');
+        // add new option for each language and check if it matches the preferred language
+        for($i = 0; $i <= 2; ++$i) {
+            echo "<option value='" . $array[$i] ."'" . (strcmp($language,$array[$i])==0 ? 'selected' : '') . ">" . $tr[$i] . "</option>";
+        }
+    }
+}
