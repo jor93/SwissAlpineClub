f2484c671ebf161d47c24ba8daf8a71b772196ad
Landiant <michael.varonier@gmail.com>
2016-10-18 10:37:28 +0200

    baba

--- controllers/Class.tourController.php
+++ controllers/Class.tourController.php
@@ -24,14 +24,18 @@ class tourController extends Controller
             && isset($_POST['artime']) && isset($_FILES['img'])){
 
             $handle = $_FILES['img']['tmp_name'];
-            Tour::insertTourImage(2, $handle);
+            $mime = $_FILES['img']['type'];
+            var_dump($handle);
+            var_dump($mime);
+            //Tour::updateTourImage(1, $handle, $mime);
+            Tour::updateTourImage(1, 'C:\Users\Acer\Pictures\Michael\Bier\bierLogo.jpg', $mime);
 
-            $image = Tour::selectTourImage(2);
-            header("Content-Type:" . 'image/jpg');
 
-            echo $image;
+            $image = Tour::selectTourImage(1);
+            header("Content-Type:" . $image['mime']);
+
+            //var_dump($image['data']);
             //http://www.mysqltutorial.org/php-mysql-blob/
-
         }
 
     }
--- data/Class.SQL.php
+++ data/Class.SQL.php
@@ -95,4 +95,61 @@ class SQL {
 		return $result;
 	}
 
+
+    // function for select query
+    public function selectBLOB($query, $tourid){
+        echo '</br>' . 'SQL query: ';
+        var_dump($query);
+        // prepare query (sql injection)
+        $stmt = $this->_conn->prepare($query);
+        $stmt->execute(array(":id" => $tourid));
+        $stmt->bindColumn(1, $data, PDO::PARAM_LOB);
+        $stmt->bindColumn(2, $mime);
+        $stmt->fetch(PDO::FETCH_BOUND);
+        // execute query or show error
+        $result = array("mime" => $mime,
+            "data" => $data)
+        or die(print_r($this->_conn->errorInfo(), true));
+        return $result;
+    }
+
+
+    // function for insert/update/delete
+    public function executeBLOBQuery($query, $tourid, $filePath, $mime){
+        echo '</br>' . 'SQL query: ';
+        var_dump($query);
+        try {
+            // begin trans
+            $this->_conn->beginTransaction();
+            // prepare query (sql injection)
+            $stmt = $this->_conn->prepare($query);
+            //write data stream
+            $blob = fopen($filePath, 'rb');
+            $stmt->bindParam(':id', $tourid);
+            $stmt->bindParam(':mime', $mime);
+            $stmt->bindParam(':data', $blob, PDO::PARAM_LOB);
+            // execute query
+            $result = $stmt->execute();
+            // get any error
+            $e = $this->_conn->errorInfo();
+            // check error
+            if ($e[1] != null) {
+                // rollback if errors
+                $this->_conn->rollBack();
+                die(print_r($this->_conn->errorInfo(), true));
+            } else{
+                // commit if no erros
+                $this->_conn->commit();
+            }
+            // rollback if any exception thrown
+        } catch (PDOException $e) {
+            $this->_conn->rollBack();
+            die(print_r($e->getMessage(), true));
+        }
+        return $result;
+    }
+
+
+
+
 }
