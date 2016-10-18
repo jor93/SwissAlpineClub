28f2fd707add517936f87caaa0cae924aaaadc10
Landiant <michael.varonier@gmail.com>
2016-10-18 09:04:02 +0200

    baba

--- controllers/Class.tourController.php
+++ controllers/Class.tourController.php
@@ -2,13 +2,12 @@
 
 /**
  * Created by PhpStorm.
- * User: mv
- * Date: 14.10.2016
- * Time: 09:18
+ * User: Acer
+ * Date: 18.10.2016
+ * Time: 09:03
  */
 class tourController extends Controller
 {
-
     function tour(){
 
         if($this->getAdminUser()){
@@ -19,10 +18,10 @@ class tourController extends Controller
 
     function insertTour(){
         if(isset($_POST['hikeName']) && isset($_POST['difficulty']) && isset($_POST['subtitle'])
-        && isset($_POST['duration']) && isset($_POST['locationDep']) && isset($_POST['locationArriv'])
-        && isset($_POST['price']) && isset($_POST['descDE']) && isset($_POST['descFR'])
-        && isset($_POST['sdate']) && isset($_POST['edate']) && isset($_POST['deptime'])
-        && isset($_POST['artime']) && isset($_FILES['img'])){
+            && isset($_POST['duration']) && isset($_POST['locationDep']) && isset($_POST['locationArriv'])
+            && isset($_POST['price']) && isset($_POST['descDE']) && isset($_POST['descFR'])
+            && isset($_POST['sdate']) && isset($_POST['edate']) && isset($_POST['deptime'])
+            && isset($_POST['artime']) && isset($_FILES['img'])){
 
             /*
             $handle = $_FILES['img']['tmp_name'];
