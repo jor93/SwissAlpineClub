4d2a75b645f60847612a65a94d73aaa30fb477a1
DeKraft <kraft.dennis@hotmail.com>
2016-10-21 14:06:23 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	css/style.css

---
+++ views/tour/tour.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gez
+ * Date: 18.10.2016
+ * Time: 08:38
+ */
+include_once 'views/header.inc';
+?>
+<script language="JavaScript" type="text/javascript">
+    $(document).ready(function () {
+        $('#menu_tour').addClass('active');
+    });
+
+</script>
+
+<div class="main-1">
+    <div class="container">
+        <div class="register">
+            <form onsubmit="return false">
+                <div class="register-top-grid">
+                    <h3>HIKE</h3>
+                    <div class="wow fadeInRight" data-wow-delay="0.4s">
+                        <span>Tour</span>
+                        <input type="text" id="tour" name="tour" required>
+                    </div>
+                    <div class="wow fadeInRight" data-wow-delay="0.4s">
+                        <span>Name</span>
+                        <input type="text" id="name" name="name" required>
+                    </div>
+
+                    <div class="wow fadeInLeft" data-wow-delay="0.4s">
+                        <span>Number of Members<label>*</label></span>
+                        <input type="number" id="member" name="member" required>
+
+                    </div>
+
+                    <div class="wow fadeInLeft" data-wow-delay="0.4s">
+                        <span>GA<label>*</label></span>
+                        <input type="number" id="ga" name="ga" required>
+
+                    </div>
+                    <div class="wow fadeInLeft" data-wow-delay="0.4s">
+                        <span>Halb-Tax<label>*</label></span>
+                        <input type="number" id="halb" name="halb" required>
+
+                    </div>
+                    <div class="wow fadeInLeft" data-wow-delay="0.4s">
+                        <span>Nothing<label>*</label></span>
+                        <input type="number" id="no" name="no" required>
+
+                    </div>
+
+
+                </div>
+                <div class="clearfix"></div>
+                <div class="register-but">
+
+                    <input type="submit" value="Show" onclick="check()">
+
+
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php
+include_once 'views/footer.inc';
+?>
