16a065566358cb9131f7d5197b3d59c062bfc80d
DeKraft <kraft.dennis@hotmail.com>
2016-11-05 22:14:16 +0100

    DK css

--- views/home.php
+++ views/home.php
@@ -53,17 +53,7 @@ include_once 'header.inc';
             <label for="ss2-item-1" class="slideshow--nav slideshow--nav-next">Go to slide 1</label>
         </div>
 
-        <form id="manage3Hikings" action="<?php echo URL_DIR.'home/';?>" method="post" enctype="multipart/form-data">
-        <section class="cd-gallery">
-            <ul>
-                <?php elementsController::getNext3Hikings(); ?>
-                <li class="gap"></li>
-                <li class="gap"></li>
-                <li class="gap"></li>
-            </ul>
-            <div class="cd-fail-message">No results found</div>
-        </section> <!-- cd-gallery -->
-        </form>
+
 
 
     </div>
@@ -72,6 +62,105 @@ include_once 'header.inc';
 
 </div>
 </body>
+<div class="curabitur">
+    <div class="container">
+        <div class="col-md-4 curabitur-top">
+            <i class="hammer"></i>
+            <h4>Contrary to popular</h4>
+            <p>Pellentesque eu congue sapien. Donec hendrerit viverra finibus. Nulla eget sollicitudin leo. Morbi lacinia libero urna necrfaucibus nulla mollis et. </p>
+            <a href="#" class="btn  btn-1c btn1 btn-1d">More</a>
+        </div>
+        <div class="col-md-4 curabitur-top">
+            <i class="timer"></i>
+            <h4>Contrary to popular</h4>
+            <p>Pellentesque eu congue sapien. Donec hendrerit viverra finibus. Nulla eget sollicitudin leo. Morbi lacinia libero urna necrfaucibus nulla mollis et. </p>
+            <a href="#" class="btn  btn-1c btn1 btn-1d">More</a>
+        </div>
+        <div class="col-md-4 curabitur-top">
+            <i class="tent"></i>
+            <h4>Contrary to popular</h4>
+            <p>Pellentesque eu congue sapien. Donec hendrerit viverra finibus. Nulla eget sollicitudin leo. Morbi lacinia libero urna necrfaucibus nulla mollis et. </p>
+            <a href="#" class="btn  btn-1c btn1 btn-1d">More</a>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+
+<!-- curabitur -->
+
+<!-- vide -->
+<div class="vide">
+    <div class="container">
+        <div class="col-md-8 vide-left">
+            <h3>Lorem ipsum dolor sit amet, consectetur a maximus metus semper et. </h3>
+            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue gravida ex,a maximus metus semper et. Curabitur non enim pharetra, dictum tortor quis, hendrerit tortor.</h6>
+            <p>Maecenas finibus quis nulla id facilisis. Aenean dignissim magna et est elit porttitor
+                gravida lacinia. Fusce elementum massa at eros lacinia imperdiet. Aenean mollis ultricies
+                dictum.Praesent suscipit urna eget elit porttitor mollis. Quisque mollis rhoncus ante, eu
+                interdum quam vehicula cursus. Nam bibendum odio eu sem semper posuere.  </p>
+        </div>
+        <div class="col-md-4 vide-right">
+            <img src="images/img1.jpg" class="img-responsive" alt="" />
+            <a class="play-icon popup-with-zoom-anim" href="#small-dialog"><span> </span></a>
+            <div id="small-dialog" class="mfp-hide">
+                <iframe src="//player.vimeo.com/video/78016933" width="" height="" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </div>
+
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<!-- vide -->
+<!-- events -->
+<div class="events">
+    <div class="container">
+        <h2>Upcoming Events</h2>
+        <div class="col-md-4 events-top">
+            <img src="images/img2.jpg" class="img-responsive" alt="" />
+            <div class="events-bottom">
+                <div class="events-left">
+                    <h5>23</h5>
+                    <span>sep</span>
+                </div>
+                <div class="events-right">
+                    <h6>Phasellus laoreet lorem nec</h6>
+                    <p>Pellentesque eu congue sapien. Donec hendrerit viverra finibus. Nulla eget sollicitudin leo. Morbi lacinia libero urna necrfaucibus nulla mollis et. </p>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="col-md-4 events-top">
+            <img src="images/img3.jpg" class="img-responsive" alt="" />
+            <div class="events-bottom">
+                <div class="events-left">
+                    <h5>06</h5>
+                    <span>jun</span>
+                </div>
+                <div class="events-right">
+                    <h6>Phasellus laoreet lorem nec</h6>
+                    <p>Pellentesque eu congue sapien. Donec hendrerit viverra finibus. Nulla eget sollicitudin leo. Morbi lacinia libero urna necrfaucibus nulla mollis et. </p>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="col-md-4 events-top">
+            <img src="images/img5.jpg" class="img-responsive" alt="" />
+            <div class="events-bottom">
+                <div class="events-left">
+                    <h5>17</h5>
+                    <span>dec</span>
+                </div>
+                <div class="events-right">
+                    <h6>Phasellus laoreet lorem nec</h6>
+                    <p>Pellentesque eu congue sapien. Donec hendrerit viverra finibus. Nulla eget sollicitudin leo. Morbi lacinia libero urna necrfaucibus nulla mollis et. </p>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+
 
 <?php
 include_once 'footer.inc';
