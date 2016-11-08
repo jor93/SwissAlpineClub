0d6afcf622992510cc9c0bdd68f729d0b7e3b236
mas18 <sandro.mathier@students.hevs.ch>
2016-11-08 09:51:59 +0100

    Sandro - added profile pic, home next 3 events

--- controllers/Class.elementsController.php
+++ controllers/Class.elementsController.php
@@ -149,11 +149,102 @@ class elementsController extends Controller
     public static function getNext3Hikings(){
         $answer = Tour::getNext3Hikings();
         $length = count($answer);
-        $img = '/' . SITE_NAME . '/images/img-6.jpg';
+
+
+
 
         for ($i = 0; $i < $length; $i++) {
             $id = $answer[$i][0];
-            echo "<li class='' onclick='showHike($id)'><img alt='No image found' src='$img' /></li>";
+            $date = $answer[$i][1];
+            $title = $answer[$i][4];
+            $subtitle = $answer[$i][5];
+            $timefrom = substr($answer[$i][6], 0, -3);
+            $timeto = substr($answer[$i][7], 0, -3);
+            $day = substr($date, -2);
+            $m = substr($date, 5, -3);
+            //month
+            switch ($m) {
+                case 1:
+                    $month = "jan";
+                    break;
+                case 2:
+                    if($_SESSION['lang'] == 'fr')
+                        $month = "fev";
+                    else
+                        $month = "feb";
+                    break;
+                case 3:
+                    if($_SESSION['lang'] == 'fr')
+                        $month = "mar";
+                    else
+                        $month = "m&auml;r";
+                    break;
+                case 4:
+                    if($_SESSION['lang'] == 'fr')
+                        $month = "avr";
+                    else
+                        $month = "apr";
+                    break;
+                case 5:
+                    $month = "mai";
+                    break;
+                case 6:
+                    if($_SESSION['lang'] == 'fr')
+                        $month = "juin";
+                    else
+                        $month = "jun";
+                    break;
+                case 7:
+                    if($_SESSION['lang'] == 'fr')
+                        $month = "juil";
+                    else
+                        $month = "jul";
+                    break;
+                case 8:
+                    if($_SESSION['lang'] == 'fr')
+                        $month = "aou";
+                    else
+                        $month = "aug";
+                    break;
+                case 9:
+                    $month = "sep";
+                    break;
+                case 10:
+                    if($_SESSION['lang'] == 'fr')
+                        $month = "oct";
+                    else
+                        $month = "okt";
+                    break;
+                case 11:
+                    $month = "nov";
+                    break;
+                case 12:
+                    if($_SESSION['lang'] == 'fr')
+                        $month = "dec";
+                    else
+                        $month = "dez";
+                    break;
+                default:
+                    $month = $m;
+                    break;
+            }
+            // get tour image from db
+            $tourImage = Tour::selectTourImage($id);
+            $temp = "data:" . $tourImage['mime'] . ";base64," . base64_encode($tourImage['data']);
+            echo "<div class='col-md-4 events-top'>";
+            echo "<img onclick='showHike($id)' alt='No image found' class='img-responsive' src='$temp' />";
+            echo "<div class='events-bottom'>";
+            echo "<div class='events-left'>";
+            echo "<h5>$day</h5>";
+            echo "<span>$month</span>";
+            echo "</div>";
+            echo "<div class='events-right'>";
+            echo "<h6>$title</h6>";
+            echo "<p>$subtitle<br />$timefrom - $timeto</p>";
+            echo "</div>";
+            echo "<div class='clearfix'> </div>";
+            echo "</div>";
+            echo "</div>";
         }
         echo "<input type='hidden' id='saver' name='showHike' value='0' />";
     }
@@ -162,7 +253,7 @@ class elementsController extends Controller
     {
         $answer = Account::selectAllAccounts();
         $length = count($answer);
-        $img = '/' . SITE_NAME . '/images/img-6.jpg';
+        $img = '/' . SITE_NAME . '/images/profile_pic.png';
 
         for ($i = 0; $i < $length; $i++) {
             $finalClass = "'mix " . $answer[$i][0] . ' ' . $answer[$i][1] . ' ' . $answer[$i][2] . ' ' . $answer[$i][3] . ' ' . $answer[$i][4] . ' ' . $answer[$i][5] . ' ' . $answer[$i][6] . ' ' . $answer[$i][7] . ' ' . substr($answer[$i][8], 0, -1) . ' ' . $answer[$i][9] . "'";
@@ -273,10 +364,10 @@ class elementsController extends Controller
         $countTours = count($idTours);
         $countFavorites = count($idFavorites);
         for ($x = 0; $x < $countTours; $x++) {
+            // get tour image from db
             $tourImage = Tour::selectTourImage($idTours[$x]);
-
-
             $temp = "data:" . $tourImage['mime'] . ";base64," . base64_encode($tourImage['data']);
+
             // set strings for filters
 
             $tourTypes = array();
